59e6a0d3d3e6b79850057ab2c03cc4b6d5f0411f
Rafael Olivi <rbolivi@hotmail.com>
2021-01-09 14:01:43 -0300

    Create Header and import font

--- App.js
+++ App.js
@@ -1,21 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './src/components/Header';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+export default class App extends Component {
+  
+  render () {
+    return (
+      <View >
+        <Header />
+        {/* <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" /> */}
+      </View>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
